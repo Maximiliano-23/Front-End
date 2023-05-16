a00ce9650b5f62ce02d8c5f7b8cf0864f04fa783
Maximiliano <maxidape44@gmail.com>
2023-05-15 21:06:08 -0300

    Se agrega el proyecto

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { NuevoUsuario } from '../model/nuevo-usuario';
+import { LoginUsuario } from '../model/login-usuario';
+import { Observable } from 'rxjs';
+import { JwtDto } from '../model/jwt-dto';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  URL = environment.URL + 'auth/';
+
+    constructor(private httpClient: HttpClient) { }
+
+ public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+   return this.httpClient.post<any>(this.URL + 'nuevo', nuevoUsuario);
+ }
+
+ public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
+   return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario)
+ }
+}
+
